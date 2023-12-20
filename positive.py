e4a163802688b9ff4f4a628eba2513ba58aec335
gireesh <aarellagireesh82@gmail.com>
2023-12-20 16:40:52 +0530

    New File has been added

---
+++ positive.py
@@ -0,0 +1,10 @@
+a=int(input('enter a number:-'))
+if a>=0:
+    b = a+a
+    print(a,'is positive number and sum is ',b)
+else:
+    c = a*a
+    print(a,'is negative number and square is ',c)
+
+
+
