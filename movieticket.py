c0a9911be8ba68e51b6683e5d0b98d81a384f0b4
gireesh <154215435+9848851965@users.noreply.github.com>
2023-12-28 22:31:49 +0530

    Update movieticket.py

--- movieticket.py
+++ movieticket.py
@@ -3,13 +3,13 @@ email=input('PLEASE ENTER YOUR VALID EMAIL ID:-')
 password=(input('PLEASE ENTER YOU VALID PASSWORD:-'))
 print(' SUCESSFULLY VERIFIED YOUR MAIL ID AND PASS WORD')
 print('WELL COME TO BOOK TICKET APPLICATION')
-print('1.KGF-200 RS')
-print('2.SALAAR-100 RS')
-print('3.PUSHPA2-300 RS')
+print('1.KGF')
+print('2.SALAAR')
+print('3.PUSHPA2')
 movieName=int(input('PLEASE SELT MOVIE NAME:-'))
-print('1.diamond for 200 rs')
-print('2.gold for 100 rs')
-print('3.silver 50 rs')
+print('1.diamond for 300 rs')
+print('2.gold for 200 rs')
+print('3.silver 100 rs')
 option=int(input('PLEASE SELECT YOUR CHOICE:-'))
 seats=int(input('PLEASE ENTER HOW MANY SEATS DO YOU WANT:-'))
 diamond=30
@@ -19,15 +19,15 @@ match option:
     case 1:
         print('now available seats are')
         out=diamond-seats
-        cal=seats*200
+        cal=seats*300
     case 2:
         print(' now available seats are')
         out=gold-seats
-        cal=seats*100
+        cal=seats*200
     case 3:
         print('now available seats are')
         out=silver-seats
-        cal=seats*300
+        cal=seats*100
 print(out)
 print(cal)
 print('SUCESSFULLY BOOKED YOUR TICKETS THANKS FOR CHOOSING THIS APPLICATION')
