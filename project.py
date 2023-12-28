b037eb813a187c7dd5e10be42be82a0c73cbd34a
gireesh <154215435+9848851965@users.noreply.github.com>
2023-12-28 13:41:51 +0530

    Add files via upload

---
+++ project.py
@@ -0,0 +1,18 @@
+print('well come to book my show')
+a=input('enter your name:-')
+print('select 1 for diamond:-200 rs')
+print('select 2 for gold:-150 rs')
+print('select 3 for silver:- 100 rs ')
+c=input('enter a choice:-')
+b=input('enter a seats:-')
+if c=='1':
+    out=(int(b)*200)
+elif c=='2':
+    out=(int(b)*150)
+elif c=='3':
+    out(int(b)*100)
+print(out)
+print('your sucessfully booked the tickets')
+
+
+
